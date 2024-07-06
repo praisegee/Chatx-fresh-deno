8564907e859b62a8d422084a0b09ac5b3ff9e0a2
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2024-07-06 06:13:20 +0100

    add to the UI

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,6 +1,6 @@
 export default function SideMenu() {
 	return (
-		<nav class="fixed top-0 left-0 h-screen w-16 border py-6">
+		<nav class="fixed top-0 left-0 h-screen w-16 border py-6 hidden md:block">
 			<ul class="flex flex-col justify-around items-center gap-10">
 				<li>
 					<i class="brand fa-brands fa-phoenix-squadron brand-active">
