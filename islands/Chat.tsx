c58a6f3a7db390865cce555adad1427fc32bd343
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2024-07-06 06:52:19 +0100

    add to the UI

--- islands/Chat.tsx
+++ islands/Chat.tsx
@@ -1,20 +1,27 @@
 export default function Chat() {
   return (
-    <div class="p-2 flex flex-col md:chat">
-      <ChatContent
-        user={{
-          id: 1,
-          name: "Adesanmi Dayo",
-          message:
-            "Hello How are you? Hello How are you? Hello How are you? Hello How are you? Hello How are you? ",
-        }}
-      />
-      <ChatContent
-        user={{ id: 2, name: "John Down", message: "I'm fine how about you?" }}
-      />
-      <ChatContent
-        user={{ id: 1, name: "Adesanmi Dayo", message: "I'm fine also." }}
-      />
+    <div class="flex flex-col md:chat justify-between items-stretch chat pb-1">
+      <div>
+        <ChatContent
+          user={{
+            id: 1,
+            name: "Adesanmi Dayo",
+            message:
+              "Hello How are you? Hello How are you? Hello How are you? Hello How are you? Hello How are you? ",
+          }}
+        />
+        <ChatContent
+          user={{
+            id: 2,
+            name: "John Down",
+            message: "I'm fine how about you?",
+          }}
+        />
+        <ChatContent
+          user={{ id: 1, name: "Adesanmi Dayo", message: "I'm fine also." }}
+        />
+      </div>
+      <ChatInput />
     </div>
   );
 }
@@ -46,6 +53,21 @@ function ChatContent({ user }: { id: number; name: string }) {
         <p class={`text-right text-xs ${isMe ? "text-base-200" : ""}`}>
           Monday 10:21pm
         </p>
+      </div>
+    </div>
+  );
+}
+
+function ChatInput() {
+  return (
+    <div class="w-full px-14 m-1 flex justify-center rounded-md outline-none p-6 text-dark-100">
+      <input
+        type="text"
+        class="border-2 border-collapse bg-slate-50 outline-none text-slate-500 px-3 border-r-0 h-10 w-full"
+      />
+      <div class="border-2 border-collapse bg-slate-50 text-slate-500 px-3 flex justify-center items-center border-l-0">
+        <i class="fa-solid fa-paper-plane">
+        </i>
       </div>
     </div>
   );
